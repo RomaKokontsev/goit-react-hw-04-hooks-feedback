3bd6e1a15d6422060dbfa521da4186b3c6ac512c
Roma Kokontsev <Romeo42@ukr.net>
2021-07-29 19:02:50 +0300

    add start file

---
+++ src/components/Wrapper/Wrapper.js
@@ -0,0 +1,12 @@
+import PropTypes from "prop-types";
+import styles from "./Wrapper.module.css";
+
+const Wrapper = ({ children }) => {
+  return <div className={styles.wrapper}>{children}</div>;
+};
+
+Wrapper.propTypes = {
+  children: PropTypes.node.isRequired,
+};
+
+export default Wrapper;
